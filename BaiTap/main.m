be05696138a8834e6661584563e258295147a7af
le-van-mai <levanmai7@gmail.com>
2023-06-10 10:26:15 +0700

    Initial Commit

---
+++ BaiTap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaiTap
+//
+//  Created by MacOS on 6/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
